702a47c2f8947ba5b173cd7d4bac208ce99fc267
giacomocusinato <giacomo.cusinato@outlook.com>
2015-11-03 15:32:01 +0100

    [ADD] ui-routing set up

--- public/app/app.js
+++ public/app/app.js
@@ -3,11 +3,16 @@
 angular.module('app', ['ui.router'])
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('movi', {
-                url: '',
-                abstract: true
+            .state('tracker', {
+                url: '/tracker',
+                template: '<h1>Tracker page!</h1>'
+            })
+            .state('signup', {
+                url: '/signup',
+                templateUrl: 'app/views/signup.html',
+                controller: 'SignupCtrl as signupCtrl'
             });
 
-        $urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/tracker');
 
 });
--- public/app/controllers/signup.ctrl.js
+++ public/app/controllers/signup.ctrl.js
@@ -1,19 +1,8 @@
     "use strict";
 
     angular.module('app')
-        .config(function($stateProvider) {
-            $stateProvider
-                .state('movi.signup', {
-                    url: '/signup',
-                    views: {
-                        'signup@': {
-                            templateUrl: 'app/views/signup.html',
-                            controller: 'SignupCtrl as signupCtrl'
-                        }
-                    }
-                });
-        })
+
 
         .controller('SignupCtrl', function() {
-            console.log('invoked controller');
+
         });
