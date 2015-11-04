29aa4bd9de3d9271e8acbc20f280d3b98f3d8d65
giacomocusinato <giacomo.cusinato@outlook.com>
2015-11-04 14:05:29 +0100

    [ADD] passport middleware setup

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,16 @@
 "use strict";
 
+var passport = require('passport');
+
 module.exports = function(app) {
+
+    app.get('/login', function(req, res) {
+        passport.authenticate('local', {
+            successRedirect: '/',
+            failureRedirect: '/login',
+            failureFlash: true
+        });
+    });
 
     app.get('*', function(req, res) {
         res.render('index.html');
