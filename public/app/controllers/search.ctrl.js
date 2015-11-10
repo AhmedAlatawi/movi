13864af8413f4d43f9f55896583dee711c5f7368
giacomocusinato <giacomo.cusinato@outlook.com>
2015-11-10 14:32:14 +0100

    [ADD] hook up movieService

--- public/app/controllers/search.ctrl.js
+++ public/app/controllers/search.ctrl.js
@@ -1,4 +1,17 @@
 angular.module('app')
-    .controller('SearchCtrl', function() {
-        
+    .controller('SearchCtrl', function(movieService) {
+        var vm = this;
+
+
+        vm.movieCollection = [];
+
+        vm.getMovies = function (query) {
+            movieService.getMoviesbyTitle(query)
+                .then(function(result) {
+                   console.log(result)
+                }, function() {
+                    console.log('error');
+                });
+        };
+
     });
