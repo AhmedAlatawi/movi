6f5a66dcef89a8aa4b16720b356c3ff2ef048300
giacomocusinato <giacomo.cusinato@outlook.com>
2015-11-11 11:21:19 +0100

    [ADD] movie service using themoviedb.org API

--- public/app/services/movie.service.js
+++ public/app/services/movie.service.js
@@ -1,7 +1,7 @@
 "use strict";
 
 angular.module('app')
-    .service('movieService', function($q, $http) {
+    .service('movieService', function($q, $http, genresService, _) {
         var service = this;
 
         var BASE_URL = 'https://api.themoviedb.org/3/',
@@ -10,10 +10,11 @@ angular.module('app')
             POSTER_SIZE_CODES = {
                 SMALL: "w154",
                 MEDIUM: "w342",
-                BIG: "w500",
+                LARGE: "w500",
                 ORIGINAL: "original"
             },
-            API_KEY='fa64f5e6000282025165656b68b67dcb';
+            API_KEY = 'fa64f5e6000282025165656b68b67dcb',
+            GENRES = genresService;
 
 
         service.getMoviesbyTitle = function(query) {
@@ -25,14 +26,47 @@ angular.module('app')
                     'query': query
                 }
             })
-                .then(function (response) {
-                    console.log(response);
-                    deferred.resolve()
-                }, function () {
-                    deferred.$$reject();
+                .then(function(response) {
+                    if (!response.data.results.length) {
+                        deferred.reject('No movies found.');
+                    } else {
+                        var movies = [];
+
+                        _.each(response.data.results, function(movie) {
+                            movies.push({
+                                id: movie.id,
+                                title: movie.original_title,
+                                description: movie.overview,
+                                poster_url: createPosterUrl(movie.poster_path, POSTER_SIZE_CODES.LARGE),
+                                genres: parseGenres(movie.genre_ids)
+                            });
+                        });
+
+                        deferred.resolve(movies);
+                    }
+
+                }, function() {
+                    deferred.$$reject('An error has occurred. Please try again later!');
                 });
 
             return deferred.promise;
         };
 
+
+        function parseGenres(ids) {
+            var genres = [];
+
+            _.each(ids, function(val) {
+                var genre = _.find(GENRES, {id: val});
+                if (!!genre) {
+                    genres.push(genre.name);
+                }
+            });
+
+            return genres;
+        }
+
+        function createPosterUrl(path, size) {
+            return POSTER_BASE_PATH + size + path;
+        }
     });
