e07fa664d1e7ea23bcfb60706c255ecf9ce0d66e
giacomocusinato <giacomo.cusinato@outlook.com>
2015-11-12 11:36:31 +0100

    [ADD] create user service and addToTracked method

---
+++ public/app/services/user.service.js
@@ -0,0 +1,24 @@
+"use strict";
+
+angular.module('app')
+    .service('userService', function($http, $q) {
+        var service = this;
+
+        service.addToTracked = function(username, movieData) {
+            var deferred = $q.defer();
+
+            $http.post('/add-tracked', {
+                username: username,
+                movieData: movieData
+            })
+                .then(function (response) {
+                    if (response.data.success) {
+                        deferred.resolve();
+                    }
+                }, function(response) {
+                    if (!response.data.success) {
+                        deferred.reject(response.data.msg);
+                    }
+                });
+        };
+    });
